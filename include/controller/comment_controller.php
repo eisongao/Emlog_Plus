57cfe8eb3d57c8d3b0ea2abd62cd55a3a2547b4e
Flyerme <gao.eison@gmail.com>
2017-08-16 16:06:33 -0400

    Add files via upload

---
+++ include/controller/comment_controller.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * 发表评论
+ *
+ * @copyright (c) Emlog All Rights Reserved
+ */
+
+class Comment_Controller {
+	function addComment($params) {
+		$name = isset($_POST['comname']) ? addslashes(trim($_POST['comname'])) : '';
+		$content = isset($_POST['comment']) ? addslashes(trim($_POST['comment'])) : '';
+		$mail = isset($_POST['commail']) ? addslashes(trim($_POST['commail'])) : '';
+		$url = isset($_POST['comurl']) ? addslashes(trim($_POST['comurl'])) : '';
+		$imgcode = isset($_POST['imgcode']) ? addslashes(trim(strtoupper($_POST['imgcode']))) : '';
+		$blogId = isset($_POST['gid']) ? intval($_POST['gid']) : -1;
+		$pid = isset($_POST['pid']) ? intval($_POST['pid']) : 0;
+
+		if (ISLOGIN === true) {
+			$CACHE = Cache::getInstance();
+			$user_cache = $CACHE->readCache('user');
+			$name = addslashes($user_cache[UID]['name_orig']);
+			$mail = addslashes($user_cache[UID]['mail']);
+			$url = addslashes(BLOG_URL);
+		}
+
+		if ($url && strncasecmp($url,'http',4)) {
+			$url = 'http://'.$url;
+		}
+
+		doAction('comment_post');
+
+		$Comment_Model = new Comment_Model();
+		$Comment_Model->setCommentCookie($name,$mail,$url);
+		if($Comment_Model->isLogCanComment($blogId) === false) {
+emMsg(langs('comment_error_comment_disabled'));
+		} elseif ($Comment_Model->isCommentExist($blogId, $name, $content) === true) {
+	emMsg(langs('comment_error_content_exists'));
+		} elseif (ROLE == ROLE_VISITOR && $Comment_Model->isCommentTooFast() === true) {
+emMsg(langs('comment_error_flood_control'));			} elseif (empty($name)) {
+		emMsg(langs('comment_error_name_enter'));
+		} elseif (strlen($name) > 20) {
+	emMsg(langs('comment_error_name_invalid'));
+		} elseif ($mail != '' && !checkMail($mail)) {
+	emMsg(langs('comment_error_email_invalid'));
+		} elseif (ISLOGIN == false && $Comment_Model->isNameAndMailValid($name, $mail) === false) {
+		emMsg(langs('comment_error_other_user'));
+		} elseif (!empty($url) && preg_match("/^(http|https)\:\/\/[^<>'\"]*$/", $url) == false) {
+emMsg(langs('comment_error_url_invalid'),'javascript:history.back(-1);');
+		} elseif (empty($content)) {
+			emMsg(lang('comment_error_empty'));
+		} elseif (strlen($content) > 8000) {
+	emMsg(langs('comment_error_content_invalid'));
+		} elseif (ROLE == ROLE_VISITOR && Option::get('comment_needchinese') == 'y' && !preg_match('/[\x{4e00}-\x{9fa5}]/iu', $content)) {
+emMsg(langs('comment_error_national_chars'));			} elseif (ISLOGIN == false && Option::get('comment_code') == 'y'  && session_start() && (empty($imgcode) || $imgcode !== $_SESSION['code']))
+ {
+	emMsg(langs('comment_error_captcha_invalid'));
+		} else {
+            $_SESSION['code'] = null;
+			$Comment_Model->addComment($name, $content, $mail, $url, $imgcode, $blogId, $pid);
+		}
+	}
+}
---
+++ include/controller/log_controller.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * 显示首页、内容
+ *
+ * @copyright (c) Emlog All Rights Reserved
+ */
+
+class Log_Controller {
+	function display($params) {
+		$Log_Model = new Log_Model();
+		$CACHE = Cache::getInstance();
+
+		$options_cache = Option::getAll();
+		extract($options_cache);
+
+		$page = isset($params[1]) && $params[1] == 'page' ? abs(intval($params[2])) : 1;
+		
+		$pageurl = '';
+		$sqlSegment ='ORDER BY top DESC ,date DESC';
+		$sta_cache = $CACHE->readCache('sta');
+		$lognum = $sta_cache['lognum'];
+		$pageurl .= Url::logPage();
+              if(isset($_GET['random'])){
+              $logarr = $Log_Model->getRandLog(1);
+              $rnd = $logarr[0]['gid'];
+              header("Location:" . Url::log($rnd));
+              exit();
+              }
+        $total_pages = ceil($lognum / $index_lognum);
+        if ($page > $total_pages) {
+            $page = $total_pages;
+        }
+		$logs = $Log_Model->getLogsForHome($sqlSegment, $page, $index_lognum);
+		$page_url = pagination($lognum, $index_lognum, $page, $pageurl);
+
+		include View::getView('header');
+		include View::getView('log_list');
+	}
+
+	function displayContent($params) {
+		$comment_page = isset($params[4]) && $params[4] == 'comment-page' ? intval($params[5]) : 1;
+
+		$Log_Model = new Log_Model();
+		$CACHE = Cache::getInstance();
+
+        $options_cache = $CACHE->readCache('options');
+		extract($options_cache);
+
+		$logid = 0 ;
+		if (isset($params[1])) {
+			if ($params[1] == 'post') {
+				$logid = isset($params[2]) ? intval($params[2]) : 0;
+			} elseif (is_numeric($params[1])) {
+				$logid = intval($params[1]);
+			} else {
+				$logalias_cache = $CACHE->readCache('logalias');
+				if (!empty($logalias_cache)) {
+					$alias = addslashes(urldecode(trim($params[1])));
+					$logid = array_search($alias, $logalias_cache);
+					if (!$logid) {
+						show_404_page();
+					}
+				}
+			}
+		}
+
+		$Comment_Model = new Comment_Model();
+
+		$logData = $Log_Model->getOneLogForHome($logid);
+		if ($logData === false) {
+			show_404_page();
+		}
+		extract($logData);
+
+		if (!empty($password)) {
+			$postpwd = isset($_POST['logpwd']) ? addslashes(trim($_POST['logpwd'])) : '';
+			$cookiepwd = isset($_COOKIE['em_logpwd_'.$logid]) ? addslashes(trim($_COOKIE['em_logpwd_'.$logid])) : '';
+			$Log_Model->AuthPassword($postpwd, $cookiepwd, $password, $logid);
+		}
+		 
+		$sort_cache = $CACHE->readCache('sort');
+                if (!isset($sort_cache[$sortid])) {
+                $log_title_style=$log_title_style>2?($log_title_style-2):$log_title_style;
+                }
+		 $sort = $sort_cache[$sortid];
+                $sortName = $sort['sortname'];
+
+		//meta
+        switch ($log_title_style) {
+            case '0':
+                $site_title = $log_title;
+                break;
+            case '1':
+                $site_title = $log_title . ' - ' . $blogname;
+                break;
+            case '2':
+                $site_title = $log_title . ' - ' . $site_title;
+                break;
+            case '3':
+                $site_title = $log_title. ' - ' . $sortName . ' - ' . $blogname;
+                break;
+           case '4':
+                $site_title = $log_title. ' - ' . $sortName  . ' - ' . $site_title;
+                break;
+        }
+		$site_description = extractHtmlData($log_content, 90);
+		$log_cache_tags = $CACHE->readCache('logtags');
+		if (!empty($log_cache_tags[$logid])) {
+			foreach ($log_cache_tags[$logid] as $value) {
+				$site_key .= ','.$value['tagname'];
+			}
+		}
+		//comments
+		$verifyCode = ISLOGIN == false && $comment_code == 'y' ? "<img src=\"".BLOG_URL."include/lib/checkcode.php\" align=\"absmiddle\" style=\"cursor : pointer;\" alt=\"".langs('view_code_no')."\" title=\"".langs('refresh_code')."\" onclick=\"this.src=this.src+'?'\" /><input name=\"imgcode\" type=\"text\" class=\"input\" size=\"5\" tabindex=\"5\" />" : '';
+		$ckname = isset($_COOKIE['commentposter']) ? htmlspecialchars(stripslashes($_COOKIE['commentposter'])) : '';
+		$ckmail = isset($_COOKIE['postermail']) ? htmlspecialchars($_COOKIE['postermail']) : '';
+		$ckurl = isset($_COOKIE['posterurl']) ? htmlspecialchars($_COOKIE['posterurl']) : '';
+		$comments = $Comment_Model->getComments(0, $logid, 'n', $comment_page);
+
+		include View::getView('header');
+		if ($type == 'blog') {
+			$Log_Model->updateViewCount($logid);
+			$neighborLog = $Log_Model->neighborLog($timestamp);
+            $tb = array();$tb_url = '';//兼容未删除引用模板
+			include View::getView('echo_log');
+		}elseif ($type == 'page') {
+            $template = !empty($template) && file_exists(TEMPLATE_PATH . $template . '.php') ? $template : 'page';
+			include View::getView($template);
+		}
+	}
+}
